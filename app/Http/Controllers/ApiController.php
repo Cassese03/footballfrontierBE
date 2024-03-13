eff56a91be6e47420b1329895311759320fd0b22
Cassese03 <90678655+Cassese03@users.noreply.github.com>
2024-03-13 15:10:30 +0100

    API Classifica Fix 1.3 e API Profilo Fix 1.11

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -637,9 +637,9 @@ class ApiController extends Controller
 
                         $statistiche_squadra = DB::connection('pgsql')->select(
                             'SELECT
-                                    (SELECT count(id) from partite   where id_squadra_vincente = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY) ) as PartiteVinte,
-                                    (SELECT count(id) from partite   where (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY)) and id_squadra_vincente != (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY)) as PartitePerse,
-                                    (SELECT count(id) from partite where (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY))) as Presenze ');
+                                    (SELECT count(id) from partite   where completata = 1 and id_squadra_vincente = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY) ) as PartiteVinte,
+                                    (SELECT count(id) from partite   where completata = 1 and (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY)) and id_squadra_vincente != (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY)) as PartitePerse,
+                                    (SELECT count(id) from partite where completata = 1 and (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $id . '\' FETCH NEXT 1 ROWS ONLY))) as Presenze ');
                         return response(array("profilo" => $profilo, "statistichePersonali" => $statistiche_personali, "statisticheSquadra" => $statistiche_squadra, "owner" => true), 200);
 
                     } else {
@@ -661,9 +661,9 @@ class ApiController extends Controller
 
                         $statistiche_squadra = DB::connection('pgsql')->select(
                             'SELECT
-                                    (SELECT count(id) from partite   where id_squadra_vincente = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY) ) as PartiteVinte,
-                                    (SELECT count(id) from partite   where (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY)) and id_squadra_vincente != (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY)) as PartitePerse,
-                                    (SELECT count(id) from partite where (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY))) as Presenze ');
+                                    (SELECT count(id) from partite   where completata = 1 and id_squadra_vincente = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY) ) as PartiteVinte,
+                                    (SELECT count(id) from partite   where completata = 1 and (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY)) and id_squadra_vincente != (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY)) as PartitePerse,
+                                    (SELECT count(id) from partite where completata = 1 and (id_squadra_casa = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY) OR id_squadra_ospite = (SELECT id_squadra from giocatori where id_utente = \'' . $dati['id'] . '\' FETCH NEXT 1 ROWS ONLY))) as Presenze ');
 
                         return response(array("profilo" => $profilo, "statistichePersonali" => $statistiche_personali, "statisticheSquadra" => $statistiche_squadra, "owner" => false), 200);
                     }
@@ -688,9 +688,9 @@ class ApiController extends Controller
                     s.nome AS Nome,
                     COALESCE((SELECT sum(gol) FROM statistiche_partita WHERE id_squadra = s.id),0) AS GolFatti,
                     COALESCE((SELECT sum(gol) FROM statistiche_partita WHERE id_partita in (select id from partite WHERE (id_squadra_casa = s.id OR id_squadra_ospite = s.id)) AND id_squadra != s.id),0) AS GolSubiti,
-                    (SELECT count(id_squadra_vincente) FROM partite WHERE id_squadra_vincente = s.id) * 3 AS Punti,
-                    (SELECT COUNT(id) FROM partite WHERE id_squadra_vincente = s.id) as PartiteVinte,
-                    (SELECT COUNT(id) FROM partite WHERE (id_squadra_ospite = s.id OR id_squadra_casa = s.id) and id_squadra_vincente != s.id) as PartitePerse,
+                    (SELECT count(id_squadra_vincente) FROM partite WHERE completata = 1 and id_squadra_vincente = s.id) * 3 AS Punti,
+                    (SELECT COUNT(id) FROM partite WHERE completata = 1 and id_squadra_vincente = s.id) as PartiteVinte,
+                    (SELECT COUNT(id) FROM partite WHERE completata = 1 and (id_squadra_ospite = s.id OR id_squadra_casa = s.id) and id_squadra_vincente != s.id) as PartitePerse,
                     (SELECT CASE WHEN id_squadra = s.id THEN true ELSE false END FROM utente WHERE id = ' . $utenti[0]->id . ') AS OWNER,
                     row_number() over(order by (SELECT count(id_squadra_vincente) FROM partite WHERE id_squadra_vincente = s.id) * 3 desc) AS position
                     FROM squadra s
